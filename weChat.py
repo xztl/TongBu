52ec0ba62163b90b91093f1ab74c020ac9484abc
DESKTOP-AMFU9RD\MO <504698295@qq.com>
2019-09-01 23:38:23 +0800

    no message

---
+++ weChat.py
@@ -0,0 +1,134 @@
+from appium import webdriver
+from time import sleep
+
+from appium.webdriver.common.touch_action import TouchAction
+
+
+class WeChat(object):
+    def __init__(self):
+        self.desired_caps = {
+            "platformName": "Android",
+            "deviceName": "MI_5",
+            "appPackage": "com.tencent.mm",
+            "appActivity": ".ui.LauncherUI",
+            "noReset": "True"
+        }
+        self.driver = webdriver.Remote('http://127.0.0.1:4723/wd/hub', self.desired_caps)
+        self.driver.implicitly_wait(1000)
+        self.name = ""
+        self.sex = ""
+        self.location = ""
+        self.weChatNum = ""
+
+    def login(self):
+        print('开始登陆')
+        loginBtn = self.driver.find_element_by_id("com.tencent.mm:id/edu")
+        loginBtn.click()
+
+        print('录入账号')
+        phoneEdt = self.driver.find_element_by_id("com.tencent.mm:id/li")
+        phoneEdt.send_keys("17308053023")
+
+        print('点击下一步')
+        loginNext = self.driver.find_element_by_id("com.tencent.mm:id/b0f")
+        loginNext.click()
+        sleep(4)
+
+        print('输入密码')
+        passwordEdt = self.driver.find_element_by_xpath(
+            "/hierarchy/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.view.View/android.widget.FrameLayout[2]/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.LinearLayout/android.widget.ScrollView/android.widget.LinearLayout/android.widget.LinearLayout[2]/android.widget.EditText")
+        passwordEdt.send_keys("Aa123456")
+
+        print('登录---->')
+        loginBtn = self.driver.find_element_by_id("com.tencent.mm:id/b0f")
+        loginBtn.click()
+        sleep(5)
+
+    # 添加好友
+    def addFriends(self):
+        sleep(30)
+        print('点击- + --->')
+        add = self.driver.find_element_by_id("com.tencent.mm:id/qk")
+        add.click()
+
+        print('点击添加好友')
+        addLayout = self.driver.find_element_by_xpath(
+            "/hierarchy/android.widget.FrameLayout/android.widget.ListView/android.widget.LinearLayout[2]")
+        addLayout.click()
+
+        goSearch = self.driver.find_element_by_id("com.tencent.mm:id/li")
+        goSearch.click()
+
+        searchEdt = self.driver.find_element_by_id("com.tencent.mm:id/li")
+        searchEdt.send_keys("jackli674297026")
+
+        searchBtn = self.driver.find_element_by_id("com.tencent.mm:id/ra")
+        searchBtn.click()
+        sleep(30)
+
+        # 获取地区
+        localTxt = self.driver.find_element_by_id("	com.tencent.mm:id/b82").text
+        self.location = localTxt
+
+        goMarkPage = self.driver.find_element_by_xpath(
+            "/hierarchy/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.view.View/android.widget.FrameLayout[1]/android.widget.FrameLayout/android.widget.RelativeLayout/android.widget.LinearLayout/android.widget.ListView/android.widget.LinearLayout[1]/android.widget.LinearLayout[1]/android.widget.LinearLayout")
+        goMarkPage.click()
+
+        friendName = self.driver.find_element_by_id("com.tencent.mm:id/b8p")
+        self.name = friendName
+        print('friendName' + friendName)
+
+        markPageBack = self.driver.find_element_by_accessibility_id("返回")
+        markPageBack.click()
+
+        print("添加好友")
+        addFriends = self.driver.find_element_by_xpath(
+            "/hierarchy/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.view.View/android.widget.FrameLayout[1]/android.widget.FrameLayout/android.widget.RelativeLayout/android.widget.LinearLayout/android.widget.ListView/android.widget.LinearLayout[4]/android.widget.LinearLayout")
+        addFriends.click()
+
+        addFriendsBack = self.driver.find_element_by_accessibility_id("返回")
+        addFriendsBack.click()
+
+        searchPageBack = self.driver.find_element_by_accessibility_id("返回")
+        searchPageBack.click()
+
+        goHome = self.driver.find_element_by_accessibility_id("返回")
+        goHome.click()
+
+    # 爬取指定朋友的数据
+    def getFriendMsg(self):
+        # 先找到好友
+        huiHua = self.driver.find_elements_by_id('com.tencent.mm:id/b9i')
+        for item in huiHua:
+            if '二之日' in item.text:
+                # 找到联系人打开聊天室
+                item.click()
+                sleep(5)
+                # 找到联系人信息界面
+                openInfo = self.driver.find_element_by_accessibility_id("聊天信息")
+                openInfo.click()
+                # 点击头像
+                headImg = self.driver.find_element_by_xpath(
+                    "//android.widget.FrameLayout[@content-desc=\"当前所在页面,聊天信息\"]/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.view.View/android.widget.FrameLayout[2]/android.widget.FrameLayout/android.widget.RelativeLayout/android.widget.LinearLayout/android.widget.ListView/android.widget.LinearLayout[1]/android.widget.RelativeLayout[1]")
+                headImg.click()
+                # 点击朋友圈相册
+                friendPhoto = self.driver.find_element_by_xpath("//*[@text='朋友圈']")
+                TouchAction(self.driver).tap(friendPhoto).perform()
+                sleep(10)
+                # 开始抓取朋友圈数据 内容ID com.tencent.mm:id/or      item点击： 	com.tencent.mm:id/ers
+                diction = dict()
+                currItem = dict()
+                # 开始循环拿列表
+                while True:
+                    flag = True
+                    self.driver.swipe(500, 800, 500, 500, 2000)
+                    sleep(5)
+                    # 拿到所有的的标题 bounds
+                    infoList = self.driver.find_elements_by_id('com.tencent.mm:id/or')
+                    for info in infoList:
+                        print(info.text)
+
+
+if __name__ == '__main__':
+    weChat = WeChat()
+    weChat.getFriendMsg()
